f0699abf091d2c6d6ffe8a337a228d8656fef397
LunaticD325 <68996159+LunaticD325@users.noreply.github.com>
2023-11-28 08:19:10 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+# include <stdlib.h>
+
+int main()
+{
+    int A[100]; 
+    int B[100];
+    int c[100];
+    char pausa = ' ';
+    int i; 
+    int a;
+    for (i = 0; i<=100; i++) {
+        a = rand() %100;
+        
+         printf("valor A: %d\n", a);
+    }
+    
+    printf("\n\n");
+    int b;
+    int j;
+    for (j = 0; j <=100; j++) {
+        b = rand() %100;
+        
+         printf("valor B: %d\n", a);
+    }
+    pausa = getchar();
+}
+    for ()
+    return 0;
+    
+}
